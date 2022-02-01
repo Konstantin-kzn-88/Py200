18ce56dc2421d5f73b4b038f1a4ac103d98c223c
Konstantin <kuznetsovkm@yandex.ru>
2022-02-01 17:36:22 +0300

    зачет

--- car_practice/py_200_exam/DoubleLikedList.py
+++ car_practice/py_200_exam/DoubleLikedList.py
@@ -4,9 +4,9 @@
 #
 # В односвязном списке должны быть реализованы следующие методы:
 #
-# __getitem__
-# __setitem__
-# __delitem__
+# __get_item__
+# __set_item__
+# __del_item__
 # __len__
 # __str__
 # __repr__
@@ -28,220 +28,207 @@
 # https://github.com/python/cpython/blob/208a7e957b812ad3b3733791845447677a704f3e/Lib/collections/__init__.py#L1174
 # https://pythobyte.com/linked-lists-in-detail-with-python-examples-single-linked-lists-7ac94da7/
 
+from typing import Any
 from collections.abc import MutableSequence
 
+
 class Node:
-    def __init__(self, data):
-        self.item = data
-        self.ref = None
+    """
+    Класс узла
+    :param _item - данные в узле (любые)
+    :param _next_item - ссылка на следующий узел
+    :param _previous_item - ссылка на предыдущий узел (для односвязного списка,
+    во всех узлах будет None)
+    """
+
+    def __init__(self, data: Any):
+        self._item = data
+        self._next_item = None
+        self._previous_item = None
+
 
 class LinkedList(MutableSequence):
+    """
+    Класс односвязного списка, наследованный от MutableSequence (collections.abc)
+    :param start_node - первый узел
+
+    Значение start_node будет None, так как связанный
+    список будет пуст в момент создания.
+    """
+
     def __init__(self):
         self.start_node = None
 
-    def traverse_list(self):
+    def make_new_list(self) -> None:
+        """Функция изготовления односвязанного списка"""
+        nums = int(input("Сколько узлов вы хотите сделать: "))
+        if nums == 0:
+            return
+        for i in range(nums):
+            value = input("Введите значение узла:")
+            self.append(value)
+
+    def bypass_list(self) -> None:
+        """
+        Функция обхода односвязного списка
+        """
         if self.start_node is None:
             print("В списке нет элементов")
             return
         else:
             n = self.start_node
             while n is not None:
-                print(n.item , " ")
-                n = n.ref
+                print(f'_item = {n._item}, _next_item = {n._next_item}, _previous_item = {n._previous_item}')
+                n = n._next_item
 
-    def insert_at_start(self, data):
-        new_node = Node(data)
-        new_node.ref = self.start_node
-        self.start_node= new_node
-
-    def insert_at_end(self, data):
+    def append(self, data: Any) -> None:
+        """
+        Функция вставки в конец связного списка
+        :param data - данные узла
+        """
         new_node = Node(data)
         if self.start_node is None:
             self.start_node = new_node
             return
         n = self.start_node
-        while n.ref is not None:
-            n= n.ref
-        n.ref = new_node
+        while n._next_item is not None:
+            n = n._next_item
+        n._next_item = new_node
 
-    def insert_after_item(self, x, data):
-
+    def insert(self, element: Any, data: Any) -> None:
+        """
+        Функция вставки после определенного элемента
+        :param data - данные узла
+        :param element - элемент списка после которого следует вставить новый узел (data)
+        """
         n = self.start_node
-        print(n.ref)
         while n is not None:
-            if n.item == x:
+            if n._item == element:
                 break
-            n = n.ref
+            n = n._next_item
         if n is None:
-            print("item not in the list")
+            print("Элемента нет в списке")
         else:
             new_node = Node(data)
-            new_node.ref = n.ref
-            n.ref = new_node
+            new_node._next_item = n._next_item
+            n._next_item = new_node
 
-    def insert_before_item(self, x, data):
-        if self.start_node is None:
-            print("List has no element")
-            return
-
-        if x == self.start_node.item:
-            new_node = Node(data)
-            new_node.ref = self.start_node
-            self.start_node = new_node
-            return
-
-        n = self.start_node
-        print(n.ref)
-        while n.ref is not None:
-            if n.ref.item == x:
-                break
-            n = n.ref
-        if n.ref is None:
-            print("item not in the list")
-        else:
-            new_node = Node(data)
-            new_node.ref = n.ref
-            n.ref = new_node
-
-    def get_count(self):
+    def __len__(self) -> Any:
         if self.start_node is None:
             return 0
         n = self.start_node
         count = 0
         while n is not None:
             count = count + 1
-            n = n.ref
+            n = n._next_item
         return count
 
-    def search_item(self, x):
+    def __delitem__(self, element) -> None:
+        """Удаление конкретного элемента"""
         if self.start_node is None:
-            print("List has no elements")
+            print("В списке нет элемента для удаления")
             return
-        n = self.start_node
-        while n is not None:
-            if n.item == x:
-                print("Item found")
-                return True
-            n = n.ref
-        print("item not found")
-        return False
 
-    def insert_at_index (self, index, data):
-        if index == 1:
-            new_node = Node(data)
-            new_node.ref = self.start_node
-            self.start_node = new_node
-        i = 1
-        n = self.start_node
-        while i < index-1 and n is not None:
-            n = n.ref
-            i = i+1
-        if n is None:
-            print("Index out of bound")
-        else:
-            new_node = Node(data)
-            new_node.ref = n.ref
-            n.ref = new_node
-
-    def delete_at_start(self):
-        if self.start_node is None:
-            print("The list has no element to delete")
-            return
-        self.start_node = self.start_node.ref
-
-    def delete_at_end(self):
-        if self.start_node is None:
-            print("The list has no element to delete")
+        if self.start_node._item == element:
+            self.start_node = self.start_node._next_item
             return
 
         n = self.start_node
-        while n.ref.ref is not None:
-            n = n.ref
-        n.ref = None
-
-    def delete_element_by_value(self, x):
-        if self.start_node is None:
-            print("The list has no element to delete")
-            return
-
-        # Deleting first node
-        if self.start_node.item == x:
-            self.start_node = self.start_node.ref
-            return
-
-        n = self.start_node
-        while n.ref is not None:
-            if n.ref.item == x:
+        while n._next_item is not None:
+            if n._next_item._item == element:
                 break
-            n = n.ref
+            n = n._next_item
 
-        if n.ref is None:
-            print("item not found in the list")
+        if n._next_item is None:
+            print("Элемент не найден в в списке")
         else:
-            n.ref = n.ref.ref
+            n._next_item = n._next_item._next_item
 
-    def reverse_linkedlist(self):
-        prev = None
+    def __getitem__(self, num_element: int):
+
+        if self.start_node is None:
+            raise Exception("Список пуст")
+        if num_element > self.__len__() - 1:
+            raise Exception("Индекс больше длины списка")
+
         n = self.start_node
+        count = 0
         while n is not None:
-            next = n.ref
-            n.ref = prev
-            prev = n
-            n = next
-        self.start_node = prev
+            if count == num_element:
+                return n._item
+            n = n._next_item
+            count += 1
 
-    def make_new_list(self):
-        nums = int(input("How many nodes do you want to create: "))
-        if nums == 0:
-            return
-        for i in range(nums):
-            value = int(input("Enter the value for the node:"))
-            self.insert_at_end(value)
+    def __setitem__(self, num_element: int, data: Any) -> None:
 
+        if self.start_node is None:
+            raise Exception("Список пуст")
+        if num_element > self.__len__() - 1:
+            raise Exception("Индекс больше длины списка")
 
-    def __len__(self):
-        return len(self.__data)
-
-    def __getitem__(self, i):
-        if isinstance(i, slice):
-            return self.__class__(self.__data[i])
-        else:
-            return self.__data[i]
-
-    def __setitem__(self, i, item):
-        self.__data[i] = item
-
-    def __delitem__(self, i):
-        del self.__data[i]
+        n = self.start_node
+        count = 0
+        while n is not None:
+            if count == num_element:
+                n._item = data
+                return
+            n = n._next_item
+            count += 1
 
     def __repr__(self):
-        return repr(self.__data)
+        return f'{self.__class__.__name__}()'
 
-    def append(self, item):
-        self.__data.append(item)
 
-    def insert(self, i, item):
-        self.__data.insert(i, item)
+class DoubleLinkedList(LinkedList):
+    def __init__(self):
+        super().__init__()
+
+    def append(self, data: Any) -> None:
+        """
+        Функция вставки в конец связного списка
+        :param data - данные узла
+        """
+        new_node = Node(data)
+        if self.start_node is None:
+            self.start_node = new_node
+            return
+        n = self.start_node
+        while n._next_item is not None:
+            n = n._next_item
+            n._previous_item = n
+        n._next_item = new_node
+        new_node._previous_item = n._next_item
+
 
 
 if __name__ == "__main__":
+    # 1. Инициализируем новый список
     new_linked_list = LinkedList()
-    new_linked_list.insert_at_end(5)
-    new_linked_list.insert_at_end(10)
-    new_linked_list.insert_at_end(15)
-    new_linked_list.traverse_list()
-    new_linked_list.insert_at_start(20)
-    new_linked_list.insert_after_item(10, 17)
-    new_linked_list.insert_before_item(17, 25)
-    new_linked_list.insert_at_index(3,8)
-    new_linked_list.get_count()
-    new_linked_list.search_item(5)
-    new_linked_list.insert_at_end(10)
-    new_linked_list.insert_at_end(20)
-    new_linked_list.insert_at_end(30)
-    new_linked_list.insert_at_end(40)
-    new_linked_list.insert_at_end(50)
-    new_linked_list.delete_at_end()
-    new_linked_list.delete_element_by_value(30)
-
-
+    # 2. Запустим функцию создания списка (!!! обязательно сделать узел "test_node" см. п.4)
+    new_linked_list.make_new_list()
+    # 3. Добавим в конец новый узел
+    new_linked_list.append("Данные нового узла")
+    # Распечатаем узлы
+    new_linked_list.bypass_list()
+    print("___Вставка___")
+    print(" " * 20)
+    # 4. Вставить узел "125" после "test_node"
+    new_linked_list.insert("test_node", "125")
+    # Распечатаем узлы
+    new_linked_list.bypass_list()
+    # 5. Выведем первый элемент по индексу
+    print(new_linked_list[1])
+    # 6. Замена данных в узле
+    print("__Замена данных в узле__")
+    print(" " * 20)
+    new_linked_list[2] = "Заменные данные в узле"
+    # Распечатаем лист
+    new_linked_list.bypass_list()
+    print("__Двусвязный список__")
+    print(" " * 20)
+    # 7. Двусвязный список унаследованный от односвязного
+    new_double_linked_list = DoubleLinkedList()
+    # 8. Запустим функцию создания списка (!!! обязательно сделать узел "test_node" см. п.4)
+    new_double_linked_list.make_new_list()
+    # Распечатаем лист
+    new_double_linked_list.bypass_list()
